ec9fc64a740f3180896fde301759d02803a51b19
ruan65 <ruan65@gmail.com>
2015-08-12 15:53:43 +0300

    load data via loader

--- app/src/main/java/premiumapp/org/yapay/fragments/CategoriesFragment.java
+++ app/src/main/java/premiumapp/org/yapay/fragments/CategoriesFragment.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +15,12 @@ import android.widget.ListView;
 import java.util.Arrays;
 
 import premiumapp.org.yapay.R;
-import premiumapp.org.yapay.Util;
 import premiumapp.org.yapay.ym_categories_tree_data_structure.CategoryAdapter;
 import premiumapp.org.yapay.ym_categories_tree_data_structure.CategoryTree;
+import premiumapp.org.yapay.ym_categories_tree_data_structure.CategoryTreeLoader;
 
-public class CategoriesFragment extends Fragment {
+public class CategoriesFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<CategoryTree> {
 
     private ListView mCategoriesList;
     private CategoryAdapter mCategoryAdapter;
@@ -35,11 +38,9 @@ public class CategoriesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        CategoryTree categoryTree = Util.getCategoryTreeFromSqliteDb(mCtx);
+        getLoaderManager().initLoader(LOADER_ID, null, this);
 
         mCategoryAdapter = new CategoryAdapter(mCtx);
-
-        mCategoryAdapter.addAll(Arrays.asList(categoryTree.getCategories()));
 
         View root = inflater.inflate(R.layout.fr_categories, container, false);
 
@@ -47,5 +48,25 @@ public class CategoriesFragment extends Fragment {
         mCategoriesList.setAdapter(mCategoryAdapter);
 
         return root;
+    }
+
+    @Override
+    public Loader<CategoryTree> onCreateLoader(int id, Bundle args) {
+        return new CategoryTreeLoader(mCtx);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<CategoryTree> loader, CategoryTree data) {
+
+        switch (loader.getId()) {
+            case LOADER_ID:
+                mCategoryAdapter.clear();
+                mCategoryAdapter.addAll(Arrays.asList(data.getCategories()));
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<CategoryTree> loader) {
     }
 }
--- app/src/main/java/premiumapp/org/yapay/ym_categories_tree_data_structure/CategoryTreeLoader.java
+++ app/src/main/java/premiumapp/org/yapay/ym_categories_tree_data_structure/CategoryTreeLoader.java
@@ -1,43 +1,29 @@
 package premiumapp.org.yapay.ym_categories_tree_data_structure;
 
-import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
 
 import premiumapp.org.yapay.Util;
 
 public class CategoryTreeLoader extends AsyncTaskLoader<CategoryTree> {
 
     private Context mCtx;
-    private CategoryTree mCategoryTree;
 
     public CategoryTreeLoader(Context context) {
         super(context);
         mCtx = context;
+        onContentChanged();
     }
 
     @Override
     public CategoryTree loadInBackground() {
-
         return Util.getCategoryTreeFromSqliteDb(mCtx);
     }
-//
-//    @Override
-//    public void deliverResult(CategoryTree data) {
-//
-//        if (isReset()) {
-//            return;
-//        }
-//        mCategoryTree = data;
-//
-//        if (isStarted()) {
-//            super.deliverResult(data);
-//        }
-//    }
-//
-//    @Override
-//    protected void onStartLoading() {
-//        if (mCategoryTree != null) {
-//            deliverResult(mCategoryTree);
-//        }
-//    }
+
+    @Override
+    protected void onStartLoading() {
+        if (takeContentChanged()) {
+            forceLoad();
+        }
+    }
 }
